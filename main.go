5eaef54cc5d90d9035e6d15f05ea8af724d2cdfa
itka0526 <itka0526@gmail.com>
2022-11-11 16:56:32 +0300

    PORT config

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"golang_project_2/src/routes"
 	"log"
+	"os"
+	"os/user"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-const PORT = "localhost:3000"
-
 func main() {
+	var PORT string = os.Getenv("PORT")
+
+	user, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	if user.Username == "itgelt" {
+		PORT = "localhost:3000"
+	}
+
 	var app *fiber.App = fiber.New()
 
 	routes.Routes(app)
